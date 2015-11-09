f1ad13644691737acf6da1eb6121e2f4900e7398
Alexander Sviridov <alexander.sviridov@gmail.com>
2015-11-09 13:11:08 +0300

    validation spec

--- spec/models/applicant_spec.rb
+++ spec/models/applicant_spec.rb
@@ -1,3 +1,4 @@
+#: encoding: utf-8
 require 'rails_helper'
 
 RSpec.describe Applicant, type: :model do
@@ -12,6 +13,35 @@ RSpec.describe Applicant, type: :model do
       subject.applicant_skills.new(skill: Fabricate(:skill))
     end
     it { is_expected.to be_valid }
+  end
+
+  describe "validations" do
+    context "cyrillic 3-part name" do
+      before(:each) do
+        subject.applicant_skills.new(skill: Fabricate(:skill))
+        subject.name = "Иван Васильевич Петров"
+      end
+
+      it { is_expected.to be_valid }
+    end
+
+    context "non cyrillic name" do
+      before(:each) do
+        subject.applicant_skills.new(skill: Fabricate(:skill))
+        subject.name = "John Doe"
+      end
+
+      it { is_expected.not_to be_valid }
+    end
+
+    context "non 3-part name" do
+      before(:each) do
+        subject.applicant_skills.new(skill: Fabricate(:skill))
+        subject.name = "Иван Петров"
+      end
+
+      it { is_expected.not_to be_valid }
+    end
   end
 
 end
